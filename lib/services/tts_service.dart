a17220637db00266b813bfabaf11c7dbb362e439
won-hj <128560356+won-hj@users.noreply.github.com>
2026-08-24 03:40:37 +0900

    Make TTS steps await speech completion

--- lib/services/tts_service.dart
+++ lib/services/tts_service.dart
@@ -1,24 +1,18 @@
 import 'package:flutter_tts/flutter_tts.dart';
 
 class TtsService {
-  final FlutterTts _tts =
-      FlutterTts();
+  final FlutterTts _tts = FlutterTts();
 
   Future<void> initialize() async {
     await _tts.setLanguage('en-US');
-
     await _tts.setSpeechRate(0.45);
-
     await _tts.setVolume(1.0);
-
     await _tts.setPitch(1.0);
+    await _tts.awaitSpeakCompletion(true);
   }
 
-  Future<void> speak(
-    String text,
-  ) async {
+  Future<void> speak(String text) async {
     await _tts.stop();
-
     await _tts.speak(text);
   }
 
